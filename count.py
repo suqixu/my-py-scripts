afd2c0470b0bf496e5d97ee092d8c37be8cc12f5
p_zaiyzhang <suqixu@126.com>
2019-12-05 17:02:09 +0800

    优化归档文件解析

--- count.py
+++ count.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # author : zhangzaiyuan
-# date : 20191129
+# date : 20191204
 # email : suqixu@126.com
 #
 # 脚本功能:
@@ -10,59 +10,87 @@
 import re
 import os
 
-group_stack = []
-group_count = {}
+
+def total_print(file_name, group_stack, group_count):
+    total = 0
+    max_key = ''
+    max_cnt = 0
+
+    for key in group_count:
+        key_cnt = group_count[key]
+        if int(key_cnt) > max_cnt:
+            max_key = key
+            max_cnt = int(key_cnt)
+
+        total += key_cnt
+        print('{}:{}'.format(key, key_cnt))
+
+    total_name = os.path.splitext(file_name)[0]
+    print('-' * 20)
+    print('{}:{} => {}:{}\n'.format(total_name, total, max_key, max_cnt))
+
+    group_stack.clear()
+    group_count.clear()
+
+
+def get_group_name(line):
+    p = re.compile(r"\[(.*)\]")
+    m = p.match(line)
+
+    if m is not None:
+        return m.group(1)
+    else:
+        return ''
+
+
+def total_line_count(line, group_stack, group_count):
+    p = re.compile(r"[^\d]+(\d+)-(\d+)(\s)*$")
+    m = p.match(line)
+    key = group_stack[0]
+
+    if m is not None:
+        start_index = int(m.group(1))
+        end_index = int(m.group(2))
+        group_count[key] += (end_index - start_index + 1)
+    else:
+        group_count[key] += 1
+
+
+def total_line(line, group_stack, group_count):
+    line = line.strip()
+    if line == '':
+        group_stack.pop()
+
+    group_name = get_group_name(line)
+    if group_name != '':
+        group_stack.append(group_name)
+        group_count.setdefault(group_name, 0)
+    else:
+        if len(group_stack) != 0:
+            total_line_count(line, group_stack, group_count)
+
+
+def total_file(path, file_name, group_stack, group_count):
+    f = open(os.path.join(path, file_name), 'r', encoding='utf-8')
+    while True:
+        line = f.readline()
+        if line == '':
+            break
+        else:
+            total_line(line, group_stack, group_count)
+    f.close()
+
 
 def main():
-    path =  r'D:\data\nutstore\wiki\生活\归档'
+    group_stack = []
+    group_count = {}
+    path = r'D:\data\nutstore\wiki\生活\归档'
     file_name_list = os.listdir(path)
+
     for file_name in file_name_list:
+        total_file(path, file_name, group_stack, group_count)
+        total_print(file_name, group_stack, group_count)
 
-
-        try:
-            f = open(os.path.join(path, file_name), 'r', encoding='utf-8')
-            while True:
-                line = f.readline()
-                if line == '':
-                    break
-                else:
-                    line = line.strip()
-                    if line == '':
-                        group_stack.pop()
-
-                    p = re.compile(r"\[(.*)\]")
-                    m = p.match(line)
-                    if m is not None:
-                        group_name = m.group(1)
-                        group_stack.append(group_name)
-                        group_count.setdefault(group_name, 0)
-                    else:
-                        if len(group_stack) == 0:
-                            continue
-
-                        p = re.compile(r"[^\d]+(\d+)-(\d+)(\s)*$")
-                        m = p.match(line)
-                        if m is not None:
-                            print('{} ==> {}'.format(group_name, line))
-                            group_count[group_stack[0]] += (int(m.group(2)) - int(m.group(1)) + 1)
-                        else:
-                            group_count[group_stack[0]] += 1
-
-                        #print(group_count[group_stack[0]])
-                    #print(line)
-
-            total = 0
-            for i in group_count:
-                total+= group_count[i]
-                print('{}:{}'.format(i, group_count[i]))
-            print('-'* 10)
-            print('共计:' + str(total) + '\n')
-            group_count.clear()
-            group_stack.clear()
-        except Exception as ex:
-            print(ex)
-        finally:
-            f.close()
 
 if __name__ == '__main__':
     main()
